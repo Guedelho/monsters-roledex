dbf7fbec44217bb1a5dbca49b389c4bac850ea05
Guedelho <mateus.guedelho@gmail.com>
2020-02-06 17:45:45 -0600

    Updates

---
+++ precache-manifest.ce7199280eb204fe9b193c6fa21592a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a8149bbe26e35db37fbb6a1e45097c",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "09b93ed8e2c0644e01dc",
+    "url": "/monsters-roledex/static/css/main.6d775d46.chunk.css"
+  },
+  {
+    "revision": "fd6ebc4e82ad40ffe41b",
+    "url": "/monsters-roledex/static/js/2.c31d5ea1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-roledex/static/js/2.c31d5ea1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09b93ed8e2c0644e01dc",
+    "url": "/monsters-roledex/static/js/main.ebee5756.chunk.js"
+  },
+  {
+    "revision": "1153fd5fb0d3fbb24a18",
+    "url": "/monsters-roledex/static/js/runtime-main.62d407ea.js"
+  }
+]);
